547dc49efff60353c0ed36a34df027e3db4908b5
li valera <valerali2010@gmail.com>
2025-08-12 16:44:24 +0700

    tetsts

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useRawInitData } from '@telegram-apps/sdk-react';
 
 function App() {
+  const initData = useRawInitData();
+  const parsedInitData = initData ? JSON.parse(initData) : null;
+  const user = parsedInitData?.user;
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +14,16 @@ function App() {
         <p>
           Проверка WebApp в Telegram
         </p>
+        {user && (
+          <div>
+            <p>User ID: {user.id}</p>
+            <p>First Name: {user.first_name}</p>
+            {user.last_name && <p>Last Name: {user.last_name}</p>}
+            {user.username && <p>Username: @{user.username}</p>}
+            {user.language_code && <p>Language Code: {user.language_code}</p>}
+          </div>
+        )
+        }
       </header>
     </div>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,26 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { init, miniApp } from '@telegram-apps/sdk';
+
+const initializeTelegramSDK = async () => {
+  try {
+    await init();
+
+
+    if (miniApp.ready.isAvailable()) {
+      await miniApp.ready();
+      console.log('Mini App готово');
+    }
+
+
+  } catch (error) {
+    console.error('Ошибка инициализации:', error);
+  }
+};
+
+
+initializeTelegramSDK();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
